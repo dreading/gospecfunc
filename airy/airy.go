52d6a9f752574ac896f6add3ffdc2e8223b0ce5d
Darren Reading <darren.reading@infinit.com.au>
2019-10-03 17:34:19 +1000

    Airy implementation (#4)
    * Airy ai/bi implementation

---
+++ airy/airy.go
@@ -0,0 +1,19 @@
+// Copyright 2019 Infin IT Pty Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package airy
+
+import (
+	"github.com/dreading/gospecfunc/airy/internal"
+)
+
+func AiryAi(z complex128) complex128 {
+	air, aii, _, _ := amos.ZAIRY(real(z), imag(z), 0, 1)
+	return complex(air, aii)
+}
+
+func AiryBi(z complex128) complex128 {
+	bir, bii, _ := amos.ZBIRY(real(z), imag(z), 0, 1)
+	return complex(bir, bii)
+}
