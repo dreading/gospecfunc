982d73f9ea1aa8df7235ad918365dc8fb9269c79
Darren Reading <darren.reading@gmail.com>
2019-10-05 21:04:16 +1000

    Restructuring utils

---
+++ integrals/integrals.go
@@ -0,0 +1,27 @@
+// Copyright 2019 Infin IT Pty Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package misc
+
+import (
+	"github.com/dreading/gospecfunc/integrals/internal/toms"
+)
+
+// Abramowitz0 computes the  Abramowitz function of order 0
+//   ∫ 0 to infinity exp( -t*t - x/t ) dt
+func Abramowitz0(x float64) float64 {
+	return toms.ABRAM0(x)
+}
+
+// Abramowitz1 computes the  Abramowitz function of order 0
+//   ∫ 0 to infinity t * exp( -t*t - x/t ) dt
+func Abramowitz1(x float64) float64 {
+	return toms.ABRAM1(x)
+}
+
+// Abramowitz2 calculates the Abramowitz function of order 2,
+//    ∫  0 to infinity (t^2) * exp( -t*t - x/t ) dt
+func Abramowitz2(x float64) float64 {
+	return toms.ABRAM2(x)
+}
---
+++ utils/utils.go
@@ -0,0 +1,16 @@
+// Copyright 2019 Infin IT Pty Ltd. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package utils
+
+import (
+	"github.com/dreading/gospecfunc/utils/internal/toms"
+)
+
+// Cheval evaluates a Chebyshev series, using the Clenshaw method
+// with Reinsch modification
+func Cheval(n int, A []float64, t float64) float64 {
+	return toms.CHEVAL(n, A, t)
+}
