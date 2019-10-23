1a5f3aa972f400c72322b61454002b66f72fbf22
Darren Reading <darren.reading@gmail.com>
2019-10-24 00:06:00 +1100

    Updated docs

--- bessel/internal/amos/doc.go
+++ bessel/internal/amos/doc.go
@@ -2,6 +2,5 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package amos implements functions from ALGORITHM 644,
-// TRANSACTIONS ON MATHEMATICAL SOFTWARE, VOL. 21, NO. 4, December, 1995, P.  388--393. AMOS, DONALD E., SANDIA NATIONAL LABORATORIES
+// Package amos implements functions from Algorithm 644, Transactions on Mathematical Software
 package amos
--- bessel/internal/toms/doc.go
+++ bessel/internal/toms/doc.go
@@ -2,7 +2,5 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package amos implements functions from ALGORITHM 757,
-// TRANSACTIONS ON MATHEMATICAL SOFTWARE, VOL. 22, NO. 3, September, 1996, P.  288--301.
-// Allan MacLeod, Dept. of Mathematics and Statistics, University of Paisley
+// Package toms implements functions from  Algorithm 757, Transactions on Mathematical Software
 package toms
