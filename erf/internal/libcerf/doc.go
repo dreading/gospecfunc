40559480506f6ba5f8c1fdf7b79ff1a2535a8ce7
Darren Reading <darren.reading@gmail.com>
2019-10-24 00:10:48 +1100

    Updated docs

---
+++ erf/internal/libcerf/doc.go
@@ -0,0 +1,6 @@
+// Copyright © 2019 Infin IT Pty Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package libcerf implements functions from libcerf by Steven G. Johnson 
+package libcerf
---
+++ erf/internal/toms/doc.go
@@ -0,0 +1,6 @@
+// Copyright © 2019 Infin IT Pty Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package toms implements functions from  Algorithm 916, Transactions on Mathematical Software
+package toms
